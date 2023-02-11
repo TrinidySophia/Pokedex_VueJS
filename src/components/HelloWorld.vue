e222186f04712eac33be42d8aa0eb67c137e1262
Trinidy Sophia <sophiatrinidy@gmail.com>
2023-02-11 15:47:32 -0400

    Versão 1.1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,136 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="tudo">
+
+<v-dialog v-model="show_dialog" width="350" >
+      <v-card v-if="selected_pokemon" class="rounded-lg" >
+        <v-container>
+          <v-row>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selected_pokemon.id}.png`"
+                :alt="selected_pokemon.name"
+              />
+            </v-col>
+            <v-col cols="8">
+              <h1>{{ get_name(selected_pokemon) }}</h1>
+              <v-chip class="mr-2" label v-for="type in selected_pokemon.types" :key="type.slot">
+              {{ type.type.name }}
+              </v-chip >
+              <v-card-text>
+                <v-text-field >Altura: {{ selected_pokemon.height * 2.54 }} cm</v-text-field>
+              <div class="mt-2">Peso: {{ selected_pokemon.weight *0.45 }} kg</div>
+            </v-card-text>
+              </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+</v-dialog>
+
+<v-container>
+    <v-flex xs12 sm6 >
+          <v-text-field
+          v-model="search" label="Digite o nome do seu Pokemon" single-line box 
+          ></v-text-field>
+        </v-flex>
+    <v-container>
+            {{ search }}
+      <v-row>
+        <v-col cols="3" 
+        v-for="pokemon in filtered_pokemons" 
+        :key="pokemon.name">
+          <v-card id="card" @click="show_pokemon(get_id(pokemon))" class="text-center">
+            <v-container>
+              <h1> {{ get_id (pokemon) }}</h1>
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id (pokemon)}.png`"
+                :alt="pokemon.name"
+              />
+              <h2>{{ get_name(pokemon) }}</h2>
+            </v-container>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+</v-container>
+
+
+</div>
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+
+  data() {
+    return {
+      pokemons: [],
+      search:"",
+      show_dialog: false,
+      selected_pokemon: null
+    }
+  },
+
+  mounted() {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=150")
+    .then((response) => {
+      this.pokemons = response.data.results
+    })
+  },
+
+  methods: {
+    get_id(pokemon) {
+      return Number(pokemon.url.split("/")[6]);
+    },
+
+    get_name(pokemon){
+      return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+    },
+    show_pokemon(id){
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((response) => {
+        this.selected_pokemon = response.data;
+        this.show_dialog = !this.show_dialog
+      });
+      
+    }
+  },
+
+  computed: {
+    filtered_pokemons() {
+      return this.pokemons.filter((item) => {
+        return item.name.includes(this.search)
+      })
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+
+#card{
+  background-color: #fff;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.tudo{
+  background: linear-gradient(-45deg, #84ffc9, #aab2ff, #eca0ff);
+  background-size: 400% 400%;
+  
+  -webkit-animation: AnimationName 11s ease infinite;
+    animation: AnimationName 11s ease infinite;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+@-webkit-keyframes AnimationName {
+    0%{background-position:51% 0%}
+    50%{background-position:50% 100%}
+    100%{background-position:51% 0%}
 }
-a {
-  color: #42b983;
+
+@keyframes AnimationName {
+    0%{background-position:51% 0%}
+    50%{background-position:50% 100%}
+    100%{background-position:51% 0%}
 }
 </style>
